e8803919995fa0ce477502cfc27e236845d688fe
ConnorBerlinWall <122120359+ConnorBerlinWall@users.noreply.github.com>
2025-05-01 16:55:41 +0000

    saving sorting exercise

--- asd-projects/sorting/index.js
+++ asd-projects/sorting/index.js
@@ -14,6 +14,7 @@ The CSS ids you will work with are:
 ///////////////////////////////////////////////////////////////////////
 
 // TODO 2: Implement bubbleSort
+//iterates over an array and determines the value of each object then swaps j and the next array if the next array has a greater value
 async function bubbleSort(array) {
     for (var i = 0; i <= array.length - 1; i++) {
         for (var j = array.length - 1; j >= i + 1; j--) {
@@ -27,6 +28,7 @@ async function bubbleSort(array) {
 }
 
 // TODO 3: Implement quickSort
+//determines if the left or right values are bigger than one another, if they aren't it partitions the array and if they are it recurses itself and changes the values of left and right until they are equal
 async function quickSort(array, left, right) {
     if(right - left > 0) {
         var index = await partition(array, left, right)
@@ -41,7 +43,9 @@ async function quickSort(array, left, right) {
 }
 
 // TODOs 4 & 5: Implement partition
-
+//creates a pivot in which is compared to the values of objects in array, if the left value is greater than the pivot it adds 1 to left
+// if the right value is less than the pivot, it subtracts 1 from right
+//If left is greater than right, it swaps the two values
 async function partition(array, left, right) {
    var pivot = array[Math.floor((right + left) / 2)].value;
     while (left < right) {
@@ -61,6 +65,7 @@ async function partition(array, left, right) {
 }
 
 // TODO 1: Implement swap
+//swaps the objects in the array thats being compared with each other
 function swap(array, i, j) {
     [array[i], array[j]] = [array[j], array[i]]
     drawSwap(array, i, j)
